053b39c3bd3d57417dc71f74569af30bf52e531b
anndrick <anndrick.la@gmail.com>
2021-05-06 13:26:47 -0500

    comidarandom

--- snake.py
+++ snake.py
@@ -10,6 +10,7 @@ Exercises
 """
 
 from turtle import *
+import random
 from random import randrange
 from freegames import square, vector
 
@@ -30,6 +31,8 @@ def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
+    food.x = randrange(-15, 15) * 10
+    food.y = randrange(-15, 15) * 10
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -40,8 +43,8 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        "food.x = randrange(-15, 15) * 10"
+        "food.y = randrange(-15, 15) * 10"
     else:
         snake.pop(0)
 
@@ -52,7 +55,7 @@ def move():
 
     square(food.x, food.y, 9, 'green')
     update()
-    ontimer(move, 100)
+    ontimer(move, random.randint(0,419))
 
 setup(420, 420, 370, 0)
 hideturtle()
